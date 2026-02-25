be35c64d7b7c998b85bed6ce9547cb558b8c638d
Cade Bagwell <4800649588@davistech.edu>
2026-02-25 08:00:32 -0700

    add movie html, css, and js

---
+++ movie.js
@@ -0,0 +1,31 @@
+const key = "fd96b612";//Variable for the key I got
+const base = "http://www.omdbapi.com/?apikey=";//Base for the fetch part
+const form = document.querySelector('form');//Getting the form from the html
+const h2 = document.querySelector('h2');//Getting the movie title area
+const h4 = document.querySelector('h4');//Getting the movie year area
+const plotTitle = document.querySelector('h3.plot');//Getting the "Movie Plot" area
+const plotData = document.querySelector('p.plot');//Getting the plot area
+const actorTitle = document.querySelector('h3.actor');//Getting the "Movie Actors" area
+const actorData = document.querySelector('p.actor');//Getting the actors area
+
+async function getMovie(movie){//Function to get the movie and set the values in the html
+    const response = await fetch(base + key + '&t=' + movie);//Getting the website with the movie
+    const result = await response.json()//Getting the values from the website
+    console.log(result);//Logging the result to the console to see the values (and to make sure the result.'s were correct)
+
+    h2.textContent = result.Title;//Setting the h2 as the result
+    h4.textContent = 'Year: ' + result.Year;//Setting the h4 as the year
+    plotTitle.textContent = 'Movie Plot';//Setting the h3 to its value
+    plotData.textContent = result.Plot;//Setting the first paragraph to the plot
+    actorTitle.textContent = 'Movie Actors';//Setting the h3 to its... Wait couldn't we technically have done an invis class and removed that here? Oh well
+    actorData.textContent = result.Actors;//Setting the second paragraph to the actors
+}
+
+form.addEventListener('submit', function(e){//Whenever the form is submitted
+    e.preventDefault();//Don't reload
+
+    const movie = form.search.value;//Variable for the movie in the form, couldn't do trim because some movies have spaces
+    form.reset();//Empty the form
+
+    getMovie(movie);//Call the getMovie function
+});
